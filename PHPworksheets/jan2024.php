a0e88a6155ea77a5a341aa13b2daa4027ce0a638
Noah Kiser <noah.kiser@gmail.com>
2023-12-29 14:36:48 -0600

    a few more kata later

--- PHPworksheets/jan2024.php
+++ PHPworksheets/jan2024.php
@@ -1,6 +1,67 @@
 <?php
 echo "<h2> Jan 2024 </h2>";
 
+/* Can you find the needle in the haystack?
+
+Write a function findNeedle() that takes an array full of junk but containing one "needle"
+
+After your function finds the needle it should return a message (as a string) that says:
+
+"found the needle at position " plus the index it found the needle, so:
+
+Example(Input --> Output)
+
+["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"] --> "found the needle at position 5" 
+Note: In COBOL, it should return "found the needle at position 6" */
+
+
+
+function findNeedle($haystack) {
+    // your code here
+    $pos = array_search('needle', $haystack);
+    if ($pos !== false){
+        return "found the needle at position $pos";
+    }
+}
+// passed with some google help
+
+
+
+/* There was a test in your class and you passed it. Congratulations!
+But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+Return True if you're better, else False!
+
+Note:
+Your points are not included in the array of your class's points. 
+For calculating the average point you may add your point to the given array! */
+
+function betterThanAverage($classPoints, $yourPoints) {
+    $avgPoints = array_sum($classPoints) / count($classPoints);
+    if($yourPoints > $avgPoints){
+        return true;
+    }else{
+        return false;
+    }
+  }
+// success! (thanks in part to google)
+
+
+/* Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers. */
+
+function even_or_odd(int $n): string {
+    if($n % 2 == 0){
+        return "Even";
+    }
+    else{
+        return "Odd";
+    }
+  }
+
+  // passed first try - with some help from google
+
 /* From codewars -- When provided with a number between 0-9, return it in words.
     Input :: 1
     Output :: "One".
