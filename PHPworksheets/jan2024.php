5bfdbef166dd9e51e80a298c02bb70e3af4bce9b
Noah Kiser <noah.kiser@gmail.com>
2023-12-29 20:54:07 -0600

    another kata

--- PHPworksheets/jan2024.php
+++ PHPworksheets/jan2024.php
@@ -1,6 +1,30 @@
 <?php
 echo "<h2> Jan 2024 </h2>";
 
+/* from codewars - Character recognition software is widely used to digitise printed texts. 
+Thus the texts can be edited, searched and stored on a computer.
+When documents (especially pretty old ones written with a typewriter),
+ are digitised character recognition softwares often make mistakes.
+Your task is correct the errors in the digitised text. You only have to handle the following mistakes:
+
+S is misinterpreted as 5
+O is misinterpreted as 0
+I is misinterpreted as 1
+The test cases contain numbers only by mistake.
+*/
+
+function correct($string) {
+    $search = array('5', '0', '1');
+    $replace = array('S', 'O', 'I');
+    return(str_replace($search, $replace, $string));
+
+}
+// **** even better code pulled from codewars solutions:
+/* function correct($string) {
+    return str_replace(["5", "0", "1"], ["S", "O", "I"], $string);
+  } */
+
+
 /* Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and 
 the paperwork has 'm' pages. Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
 
